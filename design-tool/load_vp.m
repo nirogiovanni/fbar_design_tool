7d955f554542a97a7aa7e3be5f500ce44980dad8
nirogiovanni <36417852+nirogiovanni@users.noreply.github.com>
2022-09-07 11:18:17 +0200

    Delete design-tool directory

--- design-tool/load_vp.m
+++
@@ -1,8 +0,0 @@
-function vp = load_vp(material)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-fname="./PH-VELOCITIES/"+material+".txt";
-d= importdata(fname," ");
-vp=d(:,1);
-
-end
